a74558bad9bb754e86f9033c5c7db93c3b03c94d
historydev <historyconceptart@gmail.com>
2022-03-02 01:57:11 +0300

    Fix

--- static/js/index.js
+++ static/js/index.js
@@ -94,7 +94,7 @@ myApp.setEventOnCountry(modal, (fill, country, name) => {
 
 const eventEl = document.querySelector('.event');
 
-// #Countr
+// #Country
 
 countryEl.onchange = (e) => {
     const name = countryEl.options[countryEl.selectedIndex].textContent;
